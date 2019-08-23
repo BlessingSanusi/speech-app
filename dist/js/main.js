ae338aa80dc20728de530e40ec2db8bf6fbff97c
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-08-23 16:02:50 -0500

    files added

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+//init speechsyth API
+const synth = window.speechSynthesis;
+//selecting elements
+const textForm = document.getElementById("form");
+const textInput = document.getElementById("#text-input");
+const voiceSelect = document.getElementById("#voice-select");
+const rate = document.getElementById("#rate");
+const rateValue = document.getElementById("#rate-value");
+const pitch = document.getElementById("#pitch");
+const pitchValue = document.getElementById("#pitch-value");
